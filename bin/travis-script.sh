1cc5f087a0c95eec4e80fd32f3f2e67f394862a3
Tom Hummel <tphummel@gmail.com>
2018-09-16 22:15:50 -0700

    remove commit sha from review url
    (for now). i'll add this back in a future iteration

--- bin/travis-script.sh
+++ bin/travis-script.sh
@@ -3,8 +3,8 @@
 main(){
   if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
     HUGO_ENV=production hugo
-  elif [[ $TRAVIS_PULL_REQUEST ]]; then
-    hugo --buildFuture -b "$PREVIEW_URL/$TRAVIS_PULL_REQUEST_BRANCH/$TRAVIS_COMMIT/"
+  elif [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH != "master" ]]; then
+    hugo --buildFuture -b "$PREVIEW_URL/$TRAVIS_BRANCH/"
   else
     hugo
   fi
