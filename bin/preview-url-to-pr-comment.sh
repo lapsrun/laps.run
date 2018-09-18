2b306da08fb1fa648123b3f331520883e42880e0
Tom Hummel <tphummel@gmail.com>
2018-09-17 21:53:14 -0700

    Make review url pull request comment multi line

--- bin/preview-url-to-pr-comment.sh
+++ bin/preview-url-to-pr-comment.sh
@@ -5,7 +5,10 @@
 
 main(){
   local expires_at=$(date -d "+7 days")
-  local comment="Test these changes: $PREVIEW_URL/$TRAVIS_PULL_REQUEST_BRANCH/ (expires: $expires_at)"
+  local comment="Thank you for contributing!<br />
+Preview these changes: $PREVIEW_URL/$TRAVIS_PULL_REQUEST_BRANCH/ <br />
+Expires: $expires_at (above link will no longer work)
+Note: it may take a moment for the branch to deploy. Check all pending travis builds have completed"
 
   curl \
     -H "Authorization: token $GITHUB_TOKEN" \
