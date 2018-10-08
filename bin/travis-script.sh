64d780242139430d109b397e601a5652f4b7d631
Tom Hummel <tphummel@gmail.com>
2018-10-07 17:57:24 -0700

    fix incorrect pull request check gating preview deploy (#105)
    * fix incorrect pull request check gating preview deploy
    
    * correct impossible conditional for preview build
    
    * fix conditional check gating preview deploy
    
    * fix preview deploy build cmd
    
    TRAVIS_PULL_REQUEST is either "false" or the PR number. it is never "true"

--- bin/travis-script.sh
+++ bin/travis-script.sh
@@ -6,7 +6,7 @@ main(){
     HUGO_ENV=production hugo
 
   # preparing for preview deploy via aws cli
-  elif [[ $TRAVIS_PULL_REQUEST == "true" && $TRAVIS_BRANCH != "master" ]]; then
+  elif [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
     # PREVIEW_URL is a travis env var set in travis web console
     # -b flag is the hugo base path
     hugo --buildFuture -b "$PREVIEW_URL/$TRAVIS_PULL_REQUEST_BRANCH/$TRAVIS_PULL_REQUEST_SHA/"
