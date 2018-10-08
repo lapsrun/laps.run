2073ecaf4d29fe34d7eb10f3af625e09d830dd03
Tom Hummel <tphummel@gmail.com>
2018-10-07 17:51:25 -0700

    add `shellcheck` fixes to bin/ scripts

--- bin/yaml-to-elev.sh
+++ bin/yaml-to-elev.sh
@@ -4,18 +4,18 @@
 
 main(){
   echo "file: $1"
-  local lat=$(cat $1 | yq r - latitude)
-  local lon=$(cat $1 | yq r - longitude)
+  local lat=$(yq r "$1" latitude)
+  local lon=$(yq r "$1" longitude)
   # echo "lat: $lat"
   # echo "lon: $lon"
-  local elevation=$(curl -s https://maps.googleapis.com/maps/api/elevation/json\?locations\=$lat,$lon\&key\="$GOOGLE_MAPS_TOKEN" | jq ".results[0].elevation")
+  local elevation=$(curl -s https://maps.googleapis.com/maps/api/elevation/json\?locations="$lat","$lon"\&key="$GOOGLE_MAPS_TOKEN" | jq ".results[0].elevation")
   # echo "elevation: $elevation"
-  local elevation_rounded=$(printf "%.2f" $elevation)
+  local elevation_rounded=$(printf "%.2f" "$elevation")
   echo "elevation_rounded: $elevation_rounded"
 
   # [[:space:]] is an osx sed-ism
   # see: https://superuser.com/questions/112834/how-to-match-whitespace-in-sed
-  sed -i '' "s/^elevation_meters:[[:space:]]*$/elevation_meters: $elevation_rounded/" "$1"
+  sed -i '' "s/^elevation_meters:[[:space:]]*\$/elevation_meters: $elevation_rounded/" "$1"
 }
 
-main $1
+main "$1"
