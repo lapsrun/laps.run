0a220a470fb18d4bcaf73f719695e9715af71acd
Tom Hummel <tphummel@gmail.com>
2018-10-07 19:59:50 -0700

    put the current build sha on the ops site index footer

--- bin/deploy-ops-site.sh
+++ bin/deploy-ops-site.sh
@@ -10,6 +10,7 @@ main(){
   pushd ops-website
   aws s3 cp s3://laps.run-ops-data-private/ content/ --recursive
   hugo
+  sed -i -e "s/123456abcdef/$TRAVIS_COMMIT/" public/index.html
   aws s3 cp public s3://ops.laps.run/ --recursive
   popd
 }
